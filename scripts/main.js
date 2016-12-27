b3077d595764712e428ff424bc63f5fbbd043bff
Kinga WB <kwb@MacBook-Air-Mac.local>
2016-12-27 12:16:00 +0100

    Add calback parameter

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
-	$.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	$.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?", function(a) {
 	  $(".quote-content").append(a.quote);
 	  $(".quote-author").append(a.author);
 	});
 	$('#showQuote').click(function(){
 	  $(".quote-content").empty();
 	  $(".quote-author").empty();
-	  $.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?", function(a) {
 		  $(".quote-content").append(a.quote);
 		  $(".quote-author").append(a.author);
 		});
