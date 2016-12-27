d5f5b5eb347cdcdd579c3478f75aae4aa244a2db
Kinga WB <kwb@MacBook-Air-Mac.local>
2016-12-27 12:07:08 +0100

    Try to solve problems with cors

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
-	$.getJSON("https://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	$.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json", function(a) {
 	  $(".quote-content").append(a.quote);
 	  $(".quote-author").append(a.author);
 	});
 	$('#showQuote').click(function(){
 	  $(".quote-content").empty();
 	  $(".quote-author").empty();
-	  $.getJSON("https://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	  $.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json", function(a) {
 		  $(".quote-content").append(a.quote);
 		  $(".quote-author").append(a.author);
 		});
