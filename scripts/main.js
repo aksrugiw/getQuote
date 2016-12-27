be2712c4eb6c095cac505df139e12ccda6dad358
Kinga WB <kwb@MacBook-Air-Mac.local>
2016-12-27 11:52:35 +0100

    Remove old quote then add new one

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 	$('#showQuote').click(function(){
+	  $(".quote-content").empty();
+	  $(".quote-author").empty();
 	  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(a) {
 		  $(".quote-content").append(a.quote);
 		  $(".quote-author").append(a.author);
