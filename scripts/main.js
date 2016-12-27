71b4406508dcfb3d40fe1442c6404ed75987257b
Kinga WB <kwb@MacBook-Air-Mac.local>
2016-12-27 12:03:44 +0100

    Change http to https

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
-	$.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	$.getJSON("https://quotes.stormconsultancy.co.uk/random.json", function(a) {
 	  $(".quote-content").append(a.quote);
 	  $(".quote-author").append(a.author);
 	});
 	$('#showQuote').click(function(){
 	  $(".quote-content").empty();
 	  $(".quote-author").empty();
-	  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	  $.getJSON("https://quotes.stormconsultancy.co.uk/random.json", function(a) {
 		  $(".quote-content").append(a.quote);
 		  $(".quote-author").append(a.author);
 		});
