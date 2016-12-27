751ba01ac43e1f751be26b1280e64ae480fac675
Kinga WB <kwb@MacBook-Air-Mac.local>
2016-12-27 12:00:14 +0100

    Get quote after initializing page

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+	$.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(a) {
+	  $(".quote-content").append(a.quote);
+	  $(".quote-author").append(a.author);
+	});
 	$('#showQuote').click(function(){
 	  $(".quote-content").empty();
 	  $(".quote-author").empty();
